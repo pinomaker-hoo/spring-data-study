ec9ca059ce763154760b1ae9558720ae318bf12c
pinomaker-hoo <inhoo23@naver.com>
2023-03-09 23:27:16 +0900

    Add : JDBC Update Code

--- src/test/java/pinomaker/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/pinomaker/jdbc/repository/MemberRepositoryV0Test.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import pinomaker.jdbc.domain.Member;
 
+import static org.assertj.core.api.Assertions.*;
+
+
 import java.sql.SQLException;
 
 @Slf4j
@@ -13,11 +16,16 @@ public class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member =  new Member("memberV2", 10000);
+        Member member =  new Member("memberV3", 10000);
         repository.save(member);
 
         //findById
         Member findMember = repository.findById(member.getMmeberId());
         log.info("findMmber = {}", findMember);
+        assertThat(findMember).isEqualTo(member);
+        //update
+        repository.update(member.getMmeberId(), 20000);
+        Member updateMember = repository.findById(member.getMmeberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
     }
 }
