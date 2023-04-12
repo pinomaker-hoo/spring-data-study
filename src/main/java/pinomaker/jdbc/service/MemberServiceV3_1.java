d496800cd34fb009bb2475b39b613f788ac046d8
pinomaker-hoo <inhoo23@naver.com>
2023-04-13 00:09:16 +0900

    feat : transactionManager

---
+++ src/main/java/pinomaker/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,59 @@
+package pinomaker.jdbc.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import pinomaker.jdbc.domain.Member;
+import pinomaker.jdbc.repository.MemberRepositoryV2;
+import pinomaker.jdbc.repository.MemberRepositoryV3;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepositoryV3;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepositoryV3.findById(fromId);
+        Member toMember = memberRepositoryV3.findById(toId);
+
+
+        memberRepositoryV3.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV3.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true);
+                con.close();
+            } catch (Exception e) {
+                log.info("ERROR : ", e);
+            }
+        }
+    }
+}
